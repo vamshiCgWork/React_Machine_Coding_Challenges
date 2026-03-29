eeafb0d652d57d65b1dc9f0d3e5902bffc93194e
vamshi adepu <95366752+vamshiCgWork@users.noreply.github.com>
2026-03-29 07:47:19 +0000

    debouncedSearch

--- src/components/DebouncedApiSearch.js
+++ src/components/DebouncedApiSearch.js
@@ -2,16 +2,39 @@ import React, { useState, useEffect } from "react";
 
 export default function DebouncedApiSearch() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [debounceValue, setDebounceValue] = useState("");
+  const [results, setResults] = useState([]);
+  
   const handleSearchinput = (e) => {
     setSearchTerm(e.target.value);
   };
-  useEffect(() => {});
+  useEffect(() => {
+    if (!searchTerm.trim()) {
+      setResults([]);
+      return;
+    }
+    const timer = setTimeout(async() => {
+    try {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${searchTerm}`)
+      const data = await res.json()
+      setResults(data)
+    } catch (error) {
+      console.error("Search failed", error);
+    }
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
+  console.log("data",results)
+
+  const MemoizedList = React.memo(({results})=>{
+    return <ul>
+        {results.map((item)=>{
+         return <li key={item.id}>{item.title}</li>
+        })}
+      </ul>  })
   return (
     <div className="challenge-container">
       <h2>API Search (Debounced)</h2>
       <p>searchValue:{searchTerm}</p>
-      <p>debouncevalue:{debounceValue}</p>
       <input
         type="text"
         placeholder="Search posts by title..."
@@ -19,6 +42,8 @@ export default function DebouncedApiSearch() {
         onChange={handleSearchinput}
         className="search-input"
       />
+      {console.log("results rendered")}
+  {results.length>0&& <MemoizedList results={results}/>}
     </div>
   );
 }
