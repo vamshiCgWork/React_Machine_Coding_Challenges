f1069a68f146592e8d89c55170304156b34d8a56
vamshi adepu <95366752+vamshiCgWork@users.noreply.github.com>
2026-03-31 07:54:33 +0000

    customHOok

--- src/components/LoadMore.js
+++ src/components/LoadMore.js
@@ -1,33 +1,61 @@
 import React, { useState, useEffect } from "react";
 
-export default function LoadMoreFeed() {
-  const [items, setItems] = useState([]);
-  const [page, setPage] = useState(0);
+function useFetch(url) {
+  const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
-  const fetchData = async () => {
-    if (page == 0) {
-      return;
-    }
-    setIsLoading(true);
-    try {
-      const res = await fetch(
-        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
-      );
-      const newData = await res.json();
-      console.log("new data", newData);
-      setItems((prev) => [...prev, ...newData]);
-    } catch (error) {
-      console.error("Fetch failed", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchData();
-  }, [page]);
+    setIsLoading(true);
+    setError(null);
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error("Network Response was not ok");
+        return response.json();
+      })
+      .then((userData) => setData(userData))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, [url]);
+
+  return { data, isLoading, error };
+}
+export default function LoadMoreFeed() {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const url = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`;
+  const { data, isLoading, error } = useFetch(url);
+
+  // const fetchData = async () => {
+  //   if (page == 0) {
+  //     return;
+  //   }
+  //   setIsLoading(true);
+  //   try {
+  //     const res = await fetch(
+  //       `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
+  //     );
+  //     const newData = await res.json();
+  //     console.log("new data", newData);
+  //     setItems((prev) => [...prev, ...newData]);
+  //   } catch (error) {
+  //     console.error("Fetch failed", error);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, [page]);
+
+  useEffect(() => {
+    if (data) setItems((prev) => [...prev, ...data]);
+  }, [data]);
+  console.log("url", url);
+  console.log("data", data);
   console.log("items", items);
+  console.log("pages", page);
   return (
     <div className="challenge-container">
       <div className="postCard">
@@ -43,12 +71,13 @@ export default function LoadMoreFeed() {
         </ul>
 
         {isLoading && <p>Loading more posts...</p>}
-
-        {/* ✅ CHALLENGE: Create a button that increments the 'page' state */}
         <button
           className="load-more-btn"
           disabled={isLoading}
-          onClick={() => setPage((prev) => prev + 1)}
+          onClick={() => {
+            setPage((prev) => prev + 1);
+            // refetch();
+          }}
         >
           {isLoading ? "Fetching..." : "Load More"}
         </button>
